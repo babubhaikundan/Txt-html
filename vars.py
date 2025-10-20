734180f4c3a544bceb4f268a55ae46774954d1ce
𝕂𝕦𝕟𝕕𝕒𝕟 𝕐𝕒𝕕𝕒𝕧 😎 <kundanvirajbhatt@gmail.com>
2025-10-20 20:37:22 +0530

    Update API credentials and credit information

--- vars.py
+++ vars.py
@@ -3,11 +3,12 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "22182189"))
-API_HASH = environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
+API_ID = int(environ.get("API_ID", "22272180"))
+API_HASH = environ.get("API_HASH", "c1ca68108649edb2fb0b1e78fe03e780")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-CREDIT = environ.get("CREDIT", "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎")
+CREDIT = environ.get("CREDIT", "𝕂𝕦𝕟𝕕𝕒𝕟 𝕐𝕒𝕕𝕒𝕧😎")
 
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
 
