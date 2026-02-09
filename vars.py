631e40fa313b059b22f33c1b4274d344f6abc0a3
𝕂𝕦𝕟𝕕𝕒𝕟 𝕐𝕒𝕕𝕒𝕧 😎 <kundanvirajbhatt@gmail.com>
2026-02-09 19:13:21 +0530

    Remove old API credentials and clean up code

--- vars.py
+++ vars.py
@@ -3,8 +3,8 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "22272180"))
-API_HASH = environ.get("API_HASH", "c1ca68108649edb2fb0b1e78fe03e780")
+API_ID = int(environ.get("API_ID", ""))
+API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 CREDIT = environ.get("CREDIT", "𝕂𝕦𝕟𝕕𝕒𝕟 𝕐𝕒𝕕𝕒𝕧😎")
 
@@ -14,6 +14,7 @@ CREDIT = environ.get("CREDIT", "𝕂𝕦𝕟𝕕𝕒𝕟 𝕐𝕒𝕕𝕒𝕧�
 FORCE_SUB_CHANNEL = "BabuBhaiKundan"
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
 
 
 
